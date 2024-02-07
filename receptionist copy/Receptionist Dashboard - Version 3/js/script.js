fdc91d87007edf10b2b36eced51f9aaa79f45a10
Md Sahebullah <141474909+saheb-ul-lah@users.noreply.github.com>
2024-02-07 08:13:44 +0530

    Delete receptionist copy/Receptionist Dashboard - Version 3 directory

--- receptionist copy/Receptionist Dashboard - Version 3/js/script.js
+++
@@ -1,9 +0,0 @@
-const sidebarToggle = document.querySelector("#sidebar-toggle");
-sidebarToggle.addEventListener("click",function(){
-    document.querySelector("#sidebar").classList.toggle("collapsed");
-});
-
-document.querySelector(".theme-toggle").addEventListener("click",() => {
-    toggleLocalStorage();
-    toggleRootClass();
-});
